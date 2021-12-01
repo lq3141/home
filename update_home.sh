ff76ca5723b81e81fddb8aec8e38d68fff1bf757
qili <lq3141@gmail.com>
2021-12-01 13:26:53 +0800

    * separate update and install

--- update_home.sh
+++ update_home.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+inst_en="false"
+
+while [[ $# -gt 0 ]]; do
+    key=$1
+    case $key in
+        -i)
+                shift
+                inst_en="true"
+                ;;
+        *)
+                shift
+                echo "unrecognized parameter $key"
+    esac
+done
+
 ## find root directory
 #echo " - $0 -"
 dir_name_rel=$(dirname $0)
@@ -20,17 +36,14 @@ clean_file () {
     fi
 }
 
-## vim
-clean_file ~/.vim
-ln -s ${dir_name}/vim   ~/.vim
-clean_file ~/.vimrc
-ln -s ${dir_name}/vimrc ~/.vimrc
-
+## got work folder
 cd $dir_name
+
+## update external resource
 echo "Warning: Git submodule operation may fail. If so, manually launch it later."
 git submodule update --init --recursive --progress
 
-## emacs
+# emacs binary
 if [[ -e myelpa.zip ]]; then
     echo "Info   : MYELPA available already! skip download/unzip."
 else
@@ -47,56 +60,71 @@ if false; then
     fi
 fi
 
-clean_file ~/.emacs.d
-ln -s ${dir_name}/emacs.d ~/.emacs.d
-
-## git
-clean_file ~/.gitconfig
-ln -s ${dir_name}/gitconfig ~/.gitconfig
-
-## bashrc
-clean_file ~/.bashrc
-ln -s ${dir_name}/bashrc ~/.bashrc
-clean_file ~/.bash_functions
-ln -s ${dir_name}/bash_functions ~/.bash_functions
-
-## tmux
-clean_file ~/.tmux.conf
-ln -s ${dir_name}/tmux.conf ~/.tmux.conf
-
-## antlr
+# antlr
 if ! [[ -z ${HMENV_ANTLR_EN:x} ]]; then
     if ! [[ -e antlr/antlr-4.7.2-complete.jar ]]; then
         wget https://www.antlr.org/download/antlr-4.7.2-complete.jar
     fi
+fi
+
+
+## install
+if [[ "$inst_en" == "true" ]]; then
+    # vim
+    clean_file ~/.vim
+    ln -s ${dir_name}/vim   ~/.vim
+    clean_file ~/.vimrc
+    ln -s ${dir_name}/vimrc ~/.vimrc
+
+    # emacs
+    clean_file ~/.emacs.d
+    ln -s ${dir_name}/emacs.d ~/.emacs.d
+
+    # git
+    clean_file ~/.gitconfig
+    ln -s ${dir_name}/gitconfig ~/.gitconfig
+
+    # bash
+    clean_file ~/.bashrc
+    ln -s ${dir_name}/bashrc ~/.bashrc
+    clean_file ~/.bash_functions
+    ln -s ${dir_name}/bash_functions ~/.bash_functions
+
+    # tmux
+    clean_file ~/.tmux.conf
+    ln -s ${dir_name}/tmux.conf ~/.tmux.conf
+
+    # antlr
     clean_file ~/.antlr
     ln -s ${dir_name}/antlr ~/.antlr
     source ~/.antlr/setup.sh
+
+    ## for future tools install
+    mkdir -p ~/tools/local
+
+    # maven
+    if false; then
+        mkdir -p ~/tools/maven
+        if ! [[ -e apache-maven-3.6.1-bin.tar.gz ]]; then
+            wget http://mirror.bit.edu.cn/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+        fi
+        if ! [[ -d ~/tools/maven/apache-maven-3.6.1 ]]; then
+            tar zxvf apache-maven-3.6.1-bin.tar.gz -C ~/tools/maven
+        fi
+    fi
+
+    # module
+    if false; then
+        # module could installed by apt, not compile by myself
+        if ! [[ -d ~/tools/modules ]]; then
+            echo "Warning: module not exist, need install module as below"
+            echo "         cd modules"
+            echo "         ./installit.sh"
+            echo "         BE CAREFUL WITH THE DEPENDENCY, read readme"
+        fi
+    fi
 fi
 
-## local
-## for future tools install
-mkdir -p ~/tools/local
-
-## maven
-if false; then
-    mkdir -p ~/tools/maven
-    if ! [[ -e apache-maven-3.6.1-bin.tar.gz ]]; then
-        wget http://mirror.bit.edu.cn/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
-    fi
-    if ! [[ -d ~/tools/maven/apache-maven-3.6.1 ]]; then
-        tar zxvf apache-maven-3.6.1-bin.tar.gz -C ~/tools/maven
-    fi
-fi
-
-## module
-if false; then
-    if ! [[ -d ~/tools/modules ]]; then
-        echo "Warning: module not exist, need install module as below"
-        echo "         cd modules"
-        echo "         ./installit.sh"
-        echo "         BE CAREFUL WITH THE DEPENDENCY, read readme"
-    fi
-fi
-
+##
 cd -
+
